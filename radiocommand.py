2e4a95e6075157981e72e14943f03a3815140758
Xavier Collantes <collantes.xavier@gmail.com>
2022-02-15 23:45:06 -0800

    Add radio command

---
+++ radiocommand.py
@@ -0,0 +1,98 @@
+"""Command line interface for looking up callsigns."""
+
+from bs4 import BeautifulSoup as Soup
+import requests
+import time
+from getpass import getpass
+
+
+def main():
+    print("[Starting RADIOCOMMAND]")
+    print("[Creating a session...]\n")
+
+    radio = RadioCommandSession()
+    email: str = input("Enter QRZ.com email: ")
+    password: str = getpass("Enter QRZ.com password: ")
+    try:
+        radio.loginQrz(email, password)
+    except Exception as e:
+        print(f"Error: Could not login. {e}")
+    time.sleep(2)
+    try:
+        while True:
+            callsign = input("[Enter callsign]: ")
+            ham = radio.queryCallsign(callsign)
+            print(f"NAME: {ham['name']}")
+            print(f"CALLSIGN: {ham['callsign']}")
+            print(f"ADDRESS: {ham['address']}")
+            print(f"EMAIL: {ham['email']}")
+            print(f"NATION: {ham['nation']}")
+            print(f"PIC LINK: {ham['profile_pic']}")
+            print()
+    except KeyboardInterrupt:
+        print("[Logging out]")
+
+
+class RadioCommandSession:
+
+    def __init__(self):
+        self.session: requests.Session = requests.Session()
+
+    def loginQrz(self, login: str, password: str):
+        payload = {"username": login, "password": password}
+        self.session.post("https://www.qrz.com/login", data=payload)
+
+    def queryCallsign(self, callsign: str) -> str:
+        # Alternate url
+        # "https://www.qrz.com/lookup?tquery={callsign}&mode=callsign"
+        QRZ: str = f"https://www.qrz.com/db/{callsign}"
+        response = self.session.get(QRZ)
+
+        soup = Soup(response.text, "html.parser")
+        try:
+            name = soup.find(
+                style="color: black; font-weight: bold").get_text()
+        except AttributeError:
+            name = None
+            pass
+
+        try:
+            address = soup.find(
+                style="color: #666; font-weight: normal; font-size: 17px").get_text()
+        except AttributeError:
+            address = None
+            pass
+
+        try:
+            email = soup.find(id="eml").get_text()
+        except AttributeError:
+            email = None
+            pass
+
+        try:
+            nation = soup.find(span="position:relative;top:-8px;").get_text()
+        except AttributeError:
+            nation = None
+            pass
+
+        try:
+            pic = soup.find(id="mypic")
+        except AttributeError:
+            pic = None
+            pass
+
+        if "static/qrz/qrz_com200x150.jpg" in pic:  # Default profile picture
+            pic = None
+
+        return {
+            "name": name,
+            "callsign": callsign.upper(),
+            "address": address,
+            "email": email,
+            "nation": nation,
+            "profile_pic": pic
+        }
+
+
+if __name__ == "__main__":
+    main()
